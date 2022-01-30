01ee0ca797135d73e5ddca43f02768d1e832214a
YM23 <yahav666@gmail.com>
2022-01-30 18:24:39 +0200

    PANIC

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import json
 
+import gensim
 import nltk as nltk
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_selection import SelectKBest
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.metrics import f1_score, confusion_matrix, accuracy_score
 from gensim.test.utils import common_texts
 from gensim.models import Word2Vec
 from gensim.utils import tokenize
+import gensim.downloader as api
 
 
-
-
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def acquire_word_embedding():
+    return api.load("glove-wiki-gigaword-300")
 def dataReader(file_name):
     with open(file_name,'r') as corpus_file:
         corpus = [json.loads(jline) for jline in corpus_file.read().splitlines()]
@@ -26,8 +24,24 @@ def dataReader(file_name):
 def n_gram_cross_section(sentence1_words, sentence2_words, n_gram):
     cross_section=[]
     for word in sentence1_words:
-        if word in sentence2_words:
+        if word in sentence2_words and word not in cross_section:
             cross_section.append(word)
+    return cross_section
+def cosine(u, v):
+    return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+
+def n_gram_cross_section_with_word2vec(word2vec,sentence1_words, sentence2_words):
+    cross_section=set()
+    for word in sentence1_words:
+        if word in sentence2_words:
+            cross_section.add(word)
+        else:
+            for word2 in sentence2_words:
+                try:
+                    if cosine(word2vec[word],word2vec[word2])>0.9:
+                        cross_section.add(word2)
+                except:
+                    a=1
     return cross_section
 
 
@@ -38,20 +52,45 @@ def get_verbs_from_tree(tree_as_string):
         if s.label().startswith('V'):
             verbs.append(s[0])
     return verbs
-def prepareDataForClassifier(line):
-    sentence1=line['sentence1_parse']
-    sentence2=line['sentence2_parse']
+def prepareDataForClassifier(line,word2vec):
+    sentence1=line['sentence1_parse'].lower()
+    sentence2=line['sentence2_parse'].lower()
     sentence1_words=nltk.tree.Tree.fromstring(sentence1).leaves()
     sentence2_words=nltk.tree.Tree.fromstring(sentence2).leaves()
-    cross=n_gram_cross_section(sentence1_words,sentence2_words,1)
-
-
+    return (" ".join(n_gram_cross_section_with_word2vec(word2vec,sentence1_words,sentence2_words)))
 
 
 def testWithClassifier(train, test):
     data_for_train=dataReader(train)
-    for line in data_for_train:
-        prepareDataForClassifier(line)
+    cross_sections=[]
+    word2vec=acquire_word_embedding()
+    gold_labels=[]
+    count_vectorized = CountVectorizer(max_features=1000,
+                                            strip_accents='ascii',
+                                            lowercase=True)
+    tfidf_transformer = TfidfTransformer(use_idf=True)
+    for i,line in enumerate(data_for_train):
+        print(i)
+        cross_sections.append(prepareDataForClassifier(line,word2vec))
+        gold_labels.append(line['gold_label'])
+    print("acuierd data")
+    x_train_counts = count_vectorized.fit_transform(cross_sections)
+    x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
+    classifier=LogisticRegression().fit(x_train_tfidf,gold_labels)
+    print("trained")
+
+    data_for_test=dataReader(test)
+    cross_sections_test=[]
+    gold_labels_test=[]
+    for line in data_for_test:
+        cross_sections_test.append(prepareDataForClassifier(line,word2vec))
+        gold_labels_test.append(line['gold_label'])
+    x_new_counts = count_vectorized.transform(cross_sections_test)
+    x_new_tfidf = tfidf_transformer.transform(x_new_counts)
+    print("predicting")
+    predicted = classifier.predict(x_new_tfidf)
+    a=accuracy_score(gold_labels_test,predicted)
+    print(a)
 
 
 
